59f10425a1ad4ffebe029b59cbc105efdab1192e
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-03 10:53:25 +0200

    Create swap / backup dirs in test vim folder

--- tools/init_test_vim.sh
+++ tools/init_test_vim.sh
@@ -3,6 +3,9 @@ mkdir -p /tmp/vim_
 mkdir -p /tmp/cache_
 rm -rf /tmp/vim_/*
 rm -rf /tmp/cache_/*
-cd /tmp/vim_/
+mkdir -p /tmp/vim_/swap
+mkdir -p /tmp/vim_/backup
+mkdir -p /tmp/vim_/undodir
+cd /tmp/vim_/ || exit
 ln -s ~/Projets/Nvimrc/dotfiles/vim_rc_files rc
 FORCE_VIM_FOLDER=/tmp/vim_ FORCE_VIM_CACHE=/tmp/cache_ nvim -u ~/Projets/Nvimrc/dotfiles/vimrc
