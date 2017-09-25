7d9896653191777cfa173d15c320852b2332d046
Jean Cavallo <jean.cavallo@coopengo.com>
2017-09-25 22:42:58 +0200

    Use strict mode in install

--- install.sh
+++ install.sh
@@ -4,26 +4,30 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
+set -euo pipefail
+IFS=$'\n\t'
+
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="agignore bashrc bash_profile gitconfig gitignore hgignore hgrc inputrc nvimrc psqlrc tmux.conf tmux.conf.local vimrc Xdefaults xonshrc"    # list of files/folders to symlink in homedir
+IFS=' ' read -a files <<< "${files}"
 
 ##########
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+mkdir -p "$olddir"
 echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
-cd $dir || exit
+cd "$dir"
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
+for file in "${files[@]}"; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/."$file" ~/dotfiles_old/
     echo Creating symlink to "$file" in home directory.
@@ -35,7 +39,7 @@ mkdir -p ~/.config
 
 # Hangups
 mkdir -p ~/.config/hangups
-cp ~/dotfiles/hangups.conf ~/.config/hangups
+ln -s ~/dotfiles/hangups.conf ~/.config/hangups/
 
 # Create local binary folder
 mkdir -p ~/bin
@@ -70,21 +74,21 @@ mkdir -p ~/tmp
 mkdir -p ~/Projets
 
 # Build latest neovim
-cd ~/Projets || exit
+cd ~/Projets
 git clone https://github.com/neovim/neovim Neovim
 sudo apt-get install libtool libtool-bin autoconf automake cmake g++ pkg-config unzip libunibilium-dev
-cd Neovim || exit
+cd Neovim
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 mkdir -p ~/.config
 mkdir -p ~/.config/nvim
-cd ~/.config/nvim || exit
+cd ~/.config/nvim
 ln -s ~/dotfiles/nvimrc init.vim
 
 # Install hgreview
 sudo apt-get install python-pip
 pip install --user neovim
-cd ~/tmp || exit
+cd ~/tmp
 # hg clone https://bitbucket.org/techtonik/python-review/
 # cd python-review/
 # ./refresh.py
@@ -98,13 +102,13 @@ cd ~/tmp || exit
 # chmod +x ~/bin/upload.py
 
 # Install Power Line Fonts
-cd ~/tmp || exit
+cd ~/tmp
 git clone https://github.com/powerline/fonts
-cd fonts || exit
+cd fonts
 ./install.sh
 cd ..
 rm -r fonts
-cd ~ || exit
+cd ~
 
 # Install fbterm (replace tty)
 sudo apt-get install fbterm
@@ -113,13 +117,13 @@ sudo setcap 'cap_sys_tty_config+ep' "$(command -v fbterm)"
 sudo chmod u+s /usr/bin/fbterm
 
 # Handle remote neovim
-cd ~/tmp || exit
+cd ~/tmp
 git clone https://github.com/mhinz/neovim-remote.git neovim-remote
-cd neovim-remote || exit
+cd neovim-remote
 git checkout master
 chmod +x nvr
 cp nvr ~/bin
-cd ~/tmp || exit
+cd ~/tmp
 rm -rf neovim-remote
 
 # Install tmux
@@ -129,3 +133,6 @@ sudo apt-get install tmux rxvt-unicode-256color
 mkdir -p ~/.config/tmux_plug
 git clone https://github.com/tmux-plugins/tmux-yank ~/.config/tmux_plug/tmux-yank
 git clone https://github.com/tmux-plugins/tmux-copycat ~/.config/tmux_plug/tmux-copycat
+
+# Install tools
+sudo apt-get install shellcheck
