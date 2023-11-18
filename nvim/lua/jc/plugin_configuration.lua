a67df7176318ac80629128b824a288d647e19714
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-11-18 12:16:40 +0100

    Update indent lines setup

--- nvim/lua/jc/plugin_configuration.lua
+++ nvim/lua/jc/plugin_configuration.lua
@@ -102,10 +102,10 @@ function M.gitsigns()
 end
 
 function M.indent_lines()
-  require('indent_blankline').setup({
-    show_current_context = true,
-    show_current_context_start = true,
-    show_trailing_blankline_indent = false,
+  require('ibl').setup({
+    whitespace = {
+      remove_blankline_trail = true,
+    },
   })
 end
 
